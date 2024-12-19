c81f8f34f7b9f2a5307f7787aad636ac26952339
Victoria <85761361+VicVapoRub45@users.noreply.github.com>
2024-12-19 17:20:59 -0300

    Update checkEncrypterRunning.py

--- checkEncrypterRunning.py
+++ checkEncrypterRunning.py
@@ -9,7 +9,7 @@ def check_malicious_execution():
             # Verifica se o nome ou comando do processo coincide com o nome do script malicioso
             if malicious_name in process.info['name'] or \
                (process.info['cmdline'] and malicious_name in process.info['cmdline']):
-                print(f"⚠️ Processo suspeito detectado: {process.info}")
+                print(f"Processo suspeito detectado: {process.info}")
                 return True
         except (psutil.NoSuchProcess, psutil.AccessDenied):
             # Ignora processos inacessíveis
